479ca6a1bd5df966262ef47f7aa68d5d3f0b49b5
Alex <alexvdberg010109@gmail.com>
2024-09-02 23:35:23 +0200

    made use of classes

--- studio_game.rb
+++ studio_game.rb
@@ -1,8 +1,27 @@
-def say_hello(name:, health: 100)
-    "I'm #{name.capitalize} with a health of #{health}"
+class Player
+    def initialize(name, health = 100)
+        @name = name.capitalize
+        @health = health
+    end 
+
+    def drain
+        @health -= 10
+    end
+
+    def boost
+        @health += 10
+    end
+
+    def to_s = "I'm #{@name} with a health of #{@health}"
+      
 end
 
-puts say_hello(name: "finn", health: 60)
-puts say_hello(name: "lucy", health: 90)
-puts say_hello(name: "jase")
-puts say_hello(health: 125, name: "alex")
+
+player_2 = Player.new("lucy", 90)
+puts player_2
+
+player_3 = Player.new("jase")
+puts player_3
+
+player_4 = Player.new("alex", 125)
+puts player_4

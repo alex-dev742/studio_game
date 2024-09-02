b9b7da5e7b6f886ca4b128eb9c718ef3c934e3fc
Alex <alexvdberg010109@gmail.com>
2024-09-02 18:14:42 +0200

    finished methods

--- studio_game.rb
+++ studio_game.rb
@@ -1,4 +1,8 @@
-name = "Finn"
-health = 60
+def say_hello(name:, health: 100)
+    "I'm #{name.capitalize} with a health of #{health}"
+end
 
-puts "#{name}'s health is #{health}"
+puts say_hello(name: "finn", health: 60)
+puts say_hello(name: "lucy", health: 90)
+puts say_hello(name: "jase")
+puts say_hello(health: 125, name: "alex")

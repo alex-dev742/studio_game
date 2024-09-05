c548d3046f9bef7549311c69f1625d363f2fe3b3
Alex <alexvdberg010109@gmail.com>
2024-09-05 17:07:11 +0200

    added a treasure struct

--- lib/studio_game/game.rb
+++ lib/studio_game/game.rb
@@ -1,6 +1,18 @@
 class Game
   attr_reader :players, :title
 
+  Treasure = Data.define(:name, :points)
+
+  TREASURES = [
+    Treasure.new("pie", 10),
+    Treasure.new("coin", 25),
+    Treasure.new("flute", 50),
+    Treasure.new("compass", 65),
+    Treasure.new("key", 80),
+    Treasure.new("crown", 90),
+    Treasure.new("star", 100)
+  ]
+
   def initialize(title)
       @title = title
       @players = []
@@ -14,23 +26,40 @@ class Game
       rand(1..6)
   end
 
-  def play
-      puts "Before playing:"
-      puts players
-      players.each do |player|
+  def play(rounds = 1)
+
+    puts "\nLet's play #{@title}!"
+
+    puts "\nThe following treasures can be found:"
+        TREASURES.each do |treasure|
+        puts "A #{treasure.name} is worth #{treasure.points} points"
+    end
+
+    puts "Before playing:"
+    puts @players
+
+    1.upto(rounds) do |round|
+      puts "\nRound #{round}:"
+      @players.each do |player|
           number_rolled = roll_die
+          treasure = TREASURES.sample
+
           case number_rolled
           when 1..2
               player.drain
               puts "#{player.name} got drained"
           when 3..4
               puts "#{player.name} got skipped"
+              puts "Finn found a key worth 80 points"
           else
               player.boost
               puts "#{player.name} got boosted"
+              puts "Finn found a key worth 80 points"
           end
+          puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points"
       end
       puts "\nAfter playing:"
-      puts players
+      puts @players
+    end
   end
 end
